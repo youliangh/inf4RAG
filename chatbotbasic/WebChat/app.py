7b7b6d05ce7b4cee86ded555130bc36f9e59fecd
yhuang <thf5000y2k@gmail.com>
2025-10-24 00:12:35 -0400

    bug fix

--- chatbotbasic/WebChat/app.py
+++ chatbotbasic/WebChat/app.py
@@ -37,51 +37,26 @@ def create_system_message():
     current_date = now.strftime("%B %d, %Y")
 
     message = message.replace("{{CURRENT_DATE}}", current_date)
-    message = message.replace("{{TOOLS_PROMPT}}", llm_tools.get_tool_list_for_prompt())
 
     return message
 
 def generate(new_user_message, history):
-    """
-    Generate a response from the LLM to the user message while using the 
-    available tools.  The history contains a list of prior 
-    (user_message, assistant_response) pairs from the chat.
-    This function is intended to be called by a Gradio ChatInterface.
-
-    Within this function, we iteratively build up a prompt which includes
-    the complete reasoning chain of:
-
-       Question -> [Thought -> Action -> Result?] x N -> Conclusion
-
-    Ideally, we would include the Result for every Action.  For most models,
-    we quickly use up the entire context window when including the contents
-    of web pages, so we only include the Result for the most recent Action.
-
-    Note that Claude 2 supports a 100k context window, but in practice, I've
-    found that the Anthropic API will return a rate limit error if I actually 
-    try to send a large number of tokens, so unfortuantely I use the same logic
-    with Claude 2 as the other models.
-    """
-    ACTION_REGEX = r'(\n|^)Action: (.*)\[(.*)\]'
-    CONCLUSION_REGEX = r'(\n|^)Conclusion: .*'
-
-    prompt = f"Question: {new_user_message}\n\n"
+    prompt = new_user_message
 
     # full_response is displayed to the user in the ChatInterface and is the
     # same as the prompt, except it omits the Question and Result to improve
     # readability.
     full_response = ""
 
-    iteration = 1
-
+    iters = 0
     model = MODELS["vLLM"]
 
     try:
         while True:
             if verbose:
-                print("======")
-                print("PROMPT")
-                print("======")
+                print("="*80)
+                print(f"ITERATION {iters}")
+                print("="*80)
                 print(prompt)
 
             stream = model.generate(
@@ -91,50 +66,15 @@ def generate(new_user_message, history):
                 temperature=temperature
             )
 
-            partial_response = ""
-
             for chunk in stream:
                 completion = model.parse_completion(chunk)
 
                 if completion:
                     # Stream each completion to the ChatInterface
                     full_response += completion
-                    partial_response += completion
                     yield full_response
-
-                    # When we find an Action in the response, stop the
-                    # generation, run the tool specified in the Action,
-                    # and create a new prompt that includes the Results.
-                    matches = re.search(ACTION_REGEX, partial_response)
-                    if matches:
-                        tool = matches.group(2).strip()
-                        params = matches.group(3).strip()
-                        
-                        result = llm_tools.run_tool(tool, params)
-
-                        prompt = f"Question: {new_user_message}\n\n"
-                        prompt += f"{full_response}\n\n"
-
-                        prompt += f"Result: {result}\n\n"
-
-                        break
-
-            # Stop when we either see the Conclusion or we cannot find an 
-            # Action in the response
-            if re.search(CONCLUSION_REGEX, partial_response) or not re.search(ACTION_REGEX, partial_response):
-                return
-                        
-            if not partial_response.endswith("\n"):
-                full_response += "\n\n"
-                yield full_response
             
-            # Stop when we've exceeded max_actions
-            if iteration >= max_actions:
-                full_response += f"<span style='color:red'>*Stopping after running {max_actions} actions.*</span>"
-                yield full_response
-                return
-            else:
-                iteration += 1
+            return
     
     except Exception as e:
         full_response += f"\n<span style='color:red'>Error: {e}</span>"
@@ -144,9 +84,9 @@ def generate(new_user_message, history):
 # Create Gradio app
 system_message = create_system_message()
 if verbose:
-    print("==============")
-    print("SYSTEM MESSAGE")
-    print("==============")
+    print("="*80)
+    print("SYSTEM PROMPT:")
+    print("="*80)
     print(system_message)
 
 CSS = """
